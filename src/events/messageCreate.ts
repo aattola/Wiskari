dedb1b75cab8b7dd47f205c6fe4a7f3c491c1b96
JEFFe <jeffe@jeffe.co>
2022-01-14 18:42:15 +0200

    gettistä osa 2

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -7,6 +7,24 @@ import { Nettiauto } from '../managers/nettiauto';
 const S3 = new S3Client();
 const nettiauto = new Nettiauto();
 
+function gettistä(message: Message) {
+  const idSplit = message.id.split('');
+  if (idSplit[idSplit.length - 1] === idSplit[idSplit.length - 2]) {
+    if (idSplit[idSplit.length - 2] === idSplit[idSplit.length - 3]) {
+      if (idSplit[idSplit.length - 4] === idSplit[idSplit.length - 5]) {
+        if (idSplit[idSplit.length - 5] === idSplit[idSplit.length - 6]) {
+          return message.reply(`TUPLAT HALOOOOO PENTAT ${message.id}`);
+        }
+        return message.reply(`TUPLAT HALOOOOO QUADROT ${message.id}`);
+      }
+      return message.reply(`TUPLAT HALOOOOO TRIPLAT ${message.id}`);
+    }
+    return message.reply(`TUPLAT HALOOOOO TUPLAT ${message.id}`);
+  }
+
+  message.reply(`Gettistä: ${message.id}`);
+}
+
 const messageCreate = {
   data: {
     name: 'messageCreate',
@@ -18,7 +36,12 @@ const messageCreate = {
 
     if (message.content.toLowerCase() === 'get') {
       // gettistä
-      message.reply(`Gettistä: ${message.id}`);
+      gettistä(message);
+    }
+
+    if (message.content.toLowerCase() === 'gettistä') {
+      // gettistä
+      gettistä(message);
     }
 
     if (!message.author.bot) {

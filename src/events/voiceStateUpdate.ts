6565fde3499ab82147eae4a3687b3d38048f9a88
JEFFe <jeffe@jeffe.co>
2022-06-21 00:17:19 +0300

    feat(voicestate): Jekutus simolle (anti deafen)

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -1,5 +1,6 @@
-import { Message, User, VoiceState } from 'discord.js';
+import { Message, TextChannel, User, VoiceState } from 'discord.js';
 import { analytics } from '../logging/analytics';
+import { client } from '../index';
 
 const messageCreate = {
   data: {
@@ -22,6 +23,32 @@ const messageCreate = {
       });
       return;
     }
+
+    if (member.id === '286963674990772226') {
+      if (newUser.member?.voice.deaf) {
+        // ukko 286963674990772226 on laittanut deafen moden päälle
+        if (newUser.member.voice.channel?.id === '704392425338175610') return;
+
+        setTimeout(async () => {
+          // tsekataan uusiksi onko deafen
+          const guild = await client.guilds.fetch('229499178018013184');
+          const gmember = await guild.members.fetch('286963674990772226');
+          if (!gmember.voice) return;
+          if (gmember.voice.deaf) {
+            await gmember.voice.setChannel('704392425338175610').catch(() => {
+              console.log('huutis ei oikeuksia');
+            });
+            const chatti = (await guild.channels.cache.get(
+              '229499178018013184'
+            )!) as TextChannel;
+            await chatti.send(
+              '<@286963674990772226> Häipyi afkiin koska afkasi minuutin!'
+            );
+          }
+        }, 60 * 1000);
+      }
+    }
+
     analytics.track({
       userId: member.id,
       event: 'joinChannel',

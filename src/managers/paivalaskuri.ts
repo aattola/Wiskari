27e7c20c09fe6f5559f0f184639d373aea5a2f0a
JEFFe <jeffe@jeffe.co>
2023-07-03 16:52:37 +0300

    chore(presence): tj laskin 1/24

--- src/managers/paivalaskuri.ts
+++ src/managers/paivalaskuri.ts
@@ -2,7 +2,7 @@ import { DateTime } from 'luxon';
 
 export function laskePaivat() {
   const now = DateTime.now().setZone('Europe/Helsinki');
-  const coming = DateTime.fromISO('2023-07-03').setZone('Europe/Helsinki');
+  const coming = DateTime.fromISO('2024-01-08').setZone('Europe/Helsinki');
 
   const diff = coming.diff(now, ['hours', 'days']);
 
